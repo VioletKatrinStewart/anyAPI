68c39d28989c2c74c1afb3f97a6e7e0c97035797
Violet Stewart <vkazoo2@gmail.com>
2022-03-15 17:37:07 -0700

    second test get all passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-//const Cat = require('../lib/models/Cat');
+const Cat = require('../lib/models/Cat');
 
 describe('anyAPI routes', () => {
   beforeEach(() => {
@@ -21,6 +21,13 @@ describe('anyAPI routes', () => {
       color: 'grey',
     };
     const res = await request(app).post('/api/v1/cats').send(expected);
+
+    expect(res.body).toEqual(expected);
+  });
+
+  it('gets a list of cats', async () => {
+    const expected = await Cat.findAll();
+    const res = await request(app).get('/api/v1/cats');
 
     expect(res.body).toEqual(expected);
   });
