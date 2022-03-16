f7150ca8995cdcf318361a97997bcdc56e9c74fb
Violet Stewart <vkazoo2@gmail.com>
2022-03-15 17:09:24 -0700

    first test for creating a cat passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+//const Cat = require('../lib/models/Cat');
 
 describe('anyAPI routes', () => {
   beforeEach(() => {
@@ -10,5 +11,17 @@ describe('anyAPI routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('creates a cat', async () => {
+    const expected = {
+      id: '1',
+      name: 'Neko',
+      age: 10,
+      color: 'grey',
+    };
+    const res = await request(app).post('/api/v1/cats').send(expected);
+
+    expect(res.body).toEqual(expected);
   });
 });
