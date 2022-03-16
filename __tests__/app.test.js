75ca32eebf5bc1e450a520fc7aad11b72cbb7787
Violet Stewart <vkazoo2@gmail.com>
2022-03-16 15:32:43 -0700

    got all tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,7 +15,6 @@ describe('anyAPI routes', () => {
 
   it('creates a cat', async () => {
     const expected = {
-      id: '1',
       name: 'Neko',
       age: 10,
       color: 'grey',
@@ -26,9 +25,58 @@ describe('anyAPI routes', () => {
   });
 
   it('gets a list of cats', async () => {
-    const expected = await Cat.findAll();
+    const cat1 = await Cat.insert({
+      name: 'Neko',
+      age: 10,
+      color: 'grey',
+    });
+    const cat2 = await Cat.insert({
+      name: 'Pixie',
+      age: 10,
+      color: 'grey',
+    });
     const res = await request(app).get('/api/v1/cats');
 
-    expect(res.body).toEqual(expected);
+    expect(res.body).toEqual([cat1, cat2]);
+  });
+
+  it('gets a cat by id', async () => {
+    const cat1 = await Cat.insert({
+      name: 'Neko',
+      age: 10,
+      color: 'grey',
+    });
+    const res = await request(app).get(`/api/v1/cats/${cat1.id}`);
+
+    expect(res.body).toEqual({ id: expect.any(String), ...cat1 });
+  });
+
+  it.only('updates a cat by id', async () => {
+    await Cat.insert({
+      name: 'Neko',
+      age: 10,
+      color: 'grey',
+    });
+    const res = await request(app)
+      .patch('/api/v1/cats/1')
+      .send({ color: 'pink' });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: 'Neko',
+      age: 10,
+      color: 'pink',
+    });
+  });
+
+  it('deletes a cat by id', async () => {
+    const cat1 = await Cat.insert({
+      name: 'Neko',
+      age: 10,
+      color: 'grey',
+    });
+    const res = await request(app).delete(`/api/v1/cats/${cat1.id}`);
+
+    expect(res.body).toEqual(cat1);
   });
 });
